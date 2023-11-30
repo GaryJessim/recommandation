e6c7c1abfd186dc721ba7423463cece9ce3e0e11
Gary <gary@MacBook-Air-de-Gary.local>
2023-11-30 15:54:02 +0100

    Page d'accueil terminée

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+
+    <title>Destination HoneyMoon</title>
+</head>
+
+<body>
+    <div id='container'>
+        <h1>Bienvenue sur Destination HoneyMoon</h1>
+        <section>
+            <p>Choisissez la destination des vos rêves à l'aide de notre algorithme de recommandation.</p>
+            <p>Pour cela, répondez à quelques questions qui permettront de vous proposer les destinations en fonction
+                des
+                vos goûts.</p>
+            <a href="survey.php">Accéder au questionnaire</a>
+        </section>
+
+    </div>
+</body>
+
+</html>
